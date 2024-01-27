8dd03569ee1447f212dfcf88185ea26bd531af6f
Sebastian Soto Madrigal <s.m.sebastian.n@gmail.com>
2024-01-27 09:50:36 -0600

    authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -8,6 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:title" content="Title Here" />
+        <meta property="og:description" content="Description Here" />
+        <meta property="og:image" content="image_url_here" />
+        {/* <link rel="icon" href="path/to/favicon.ico" /> */}
+      </head>
       <body
         className={`${inter.className} antialiased`}
         suppressHydrationWarning={true}
